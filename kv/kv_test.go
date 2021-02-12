dfb54781b034116a28cae0dcca629eabe33d699c
Tianle Xu <xtl@xtlsoft.top>
2021-02-12 22:00:55 +0800

    chore(tests): kv test basic methods

--- kv/kv_test.go
+++ kv/kv_test.go
@@ -8,7 +8,6 @@ import (
 )
 
 func BenchmarkGet(b *testing.B) {
-	b.ResetTimer()
 	drv := storage.NewPebbleDriver(&storage.PebbleDriverConfigure{
 		BaseDirectory: "",
 		UseMemory:     true,
@@ -20,7 +19,33 @@ func BenchmarkGet(b *testing.B) {
 	tbl, _ := db.Table("test")
 	kv, _ := tbl.GetKV()
 	_ = kv
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		kv.Get("a")
+	}
+}
+
+func BenchmarkAnother(b *testing.B) {
+	arr := []int{0, 1, 2, 3}
+	for i := 4; i < 4000000; i++ {
+		arr = append(arr, i)
+	}
+	b.ResetTimer()
+	// arr = append([]int{1}, arr...)
+	arr2 := make([]int, len(arr)+1)
+	arr2[0] = 0
+	for i := 0; i < b.N; i++ {
+		copy(arr2[1:], arr[0:])
+	}
+}
+
+func BenchmarkAnothe2(b *testing.B) {
+	str := ""
+	for i := 1; i < 100; i++ {
+		str += "1"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = string([]byte(str))
 	}
 }
